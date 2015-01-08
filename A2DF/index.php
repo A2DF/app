9df7c9109cd0afacaa8867aea0cda6b5560a6b23
Alexandre <Alexandre@Altano-PC>
2015-01-08 15:30:45 +0100

    page index et css

--- A2DF/index.php
+++ A2DF/index.php
@@ -1,14 +1,38 @@
-<?php
-include ('html/head.html');
-?>
+<!DOCTYPE html>
 <html>
-    <link href="css/authentification.css" rel="stylesheet" type="text/css">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/style.css" />
+        <title>A2DF Informatique</title>
+    </head>
+
     <body>
-        <h1>Bienvenue sur le site d'A2DF Informatique</h1>
-        <a href='http://hugo-pc/a2df/listeAppel.php'>Liste des appels</a><br />
-        <a href='http://hugo-pc/a2df/ajoutAppel.php'>Ajout d'un appel</a>
-        <?php
-        // put your code here
-        ?>
+        <header>
+            <h1 class="logo">A2DF Informatique</h1>
+        </header>
+        
+        <nav>
+            <p><center>MENU</center></p>
+            <ul>
+                <li><a href="http://hugo-pc/a2df/listeAppel.php">Liste des appels</a></li>
+                <li><a href="http://hugo-pc/a2df/ajoutAppel.php">Ajout d'un appel</a></li>
+            </ul>
+        </nav>
+        
+        <section>
+            <aside>
+                <h1>À propos de l'auteur</h1>
+                <p>C'est moi, Zozor ! Je suis né un 23 novembre 2005.</p>
+            </aside>
+            <article>                
+                <h1>Je suis un grand voyageur</h1>
+                <p>Bla bla bla bla (texte de l'article)</p>
+            </article>
+        </section>
+        
+        <footer>
+
+        </footer>
+        
     </body>
 </html>
