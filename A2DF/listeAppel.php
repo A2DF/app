c9fb9c87773be6669fdf84d9ea0547bba0eb1da5
Hugo <Hugo@Hugo-PC>
2015-01-21 14:44:04 +0100

    popup

--- A2DF/listeAppel.php
+++ A2DF/listeAppel.php
@@ -20,56 +20,68 @@
             <div class="ribbon-back-right"></div>
         </div>
         <div class="tableaux">
-        <?php
-        //Affichage de la première ligne du tableau
-        echo "<table border='1' class='sortable'>";
-        echo "<tr>";
-        echo "<th id='date'>Date</th>";
-        echo "<th id='client'>Client</th>";
-        echo "<th id='numero'>Numero</th>";
-        echo "<th id='pour'>Pour</th>";
-        echo "<th id='motif'>Motif</th>";
-        echo "<th id='commentaire'>Priorite</th>";
-        echo "<th id='traite'>Traité</th>";
-        echo "</tr>";
-
-        $listeAppel = listeAppel();
-        foreach ($listeAppel as $appel) {
-
-            //Récupération des données dans la base
-            $date = $appel['date'];
-            $client = $appel['client'];
-            $numero = $appel['tel'];
-            $personnel = $appel['personnel'];
-            $motif = $appel['motif'];
-            $priorite = $appel['libelle'];
-
-            //Affichage des données dans le tableau
+            <?php
+            //Affichage de la première ligne du tableau
+            echo "<table border='1' class='sortable'>";
             echo "<tr>";
-            echo "<td>" . $date . "</td>";
-            echo "<td>" . $client . "</td>";
-            echo "<td>" . $numero . "</td>";
-            echo "<td>" . $personnel . "</td>";
-            echo "<td>" . $motif . "</td>";
-            echo "<td>" . $priorite . "</td>";
-            echo "<td><img src='img/tick_light_blue.png' title='Appel traité'/></td>";
+            echo "<th id='date'>Date</th>";
+            echo "<th id='client'>Client</th>";
+            echo "<th id='numero'>Numero</th>";
+            echo "<th id='pour'>Pour</th>";
+            echo "<th id='motif'>Motif</th>";
+            echo "<th id='commentaire'>Priorite</th>";
+            echo "<th id='traite'>Traité</th>";
             echo "</tr>";
-        }
-        
-        echo "</table>";
-        ?>
-    </div>
-        <a class="backtotop" href="#" onclick="backtotop();return false;"><img src="img/up6.png" onclick="backtotop();return false;" alt="Retour haut de page">
-</a>
+
+            $listeAppel = listeAppel();
+            foreach ($listeAppel as $appel) {
+
+                //Récupération des données dans la base
+                $date = $appel['date'];
+                $client = $appel['client'];
+                $numero = $appel['tel'];
+                $personnel = $appel['personnel'];
+                $motif = $appel['motif'];
+                $priorite = $appel['libelle'];
+
+                //Affichage des données dans le tableau
+                echo "<tr>";
+                echo "<td>" . $date . "</td>";
+                echo "<td>" . $client . "</td>";
+                echo "<td>" . $numero . "</td>";
+                echo "<td>" . $personnel . "</td>";
+                echo "<td>" . $motif . "</td>";
+
+                if ($priorite == "Urgent") {
+                    echo "<td class='urgent'>" . $priorite . "</td>";
+                } else if ($priorite == "Important") {
+                    echo "<td class='important'>" . $priorite . "</td>";
+                } else if ($priorite == "Normal") {
+                    echo "<td class='normal'>" . $priorite . "</td>";
+                }
+                ?>
+                <td><img src='img/tick_light_blue.png' title='Appel traité' onclick='return(confirm("Etes-vous sûr de vouloir supprimer cette entrée?"));'/></td>
+                <?php
+                echo "</tr>";
+            }
+
+            echo "</table>";
+            ?>
+        </div>
+        <a class="backtotop" href="#" onclick="backtotop();
+                return false;"><img src="img/up6.png" onclick="backtotop();
+                        return false;" alt="Retour haut de page">
+        </a>
         <script>
             var timeOut;
             function backtotop() {
-                if (document.body.scrollTop!==0 || document.documentElement.scrollTop!==0){
-                    window.scrollBy(0,-50);
-                    timeOut=setTimeout('backtotop()',25);
+                if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+                    window.scrollBy(0, -50);
+                    timeOut = setTimeout('backtotop()', 25);
                 }
-                else clearTimeout(timeOut);
+                else
+                    clearTimeout(timeOut);
             }
-            </script>
+        </script>
     </body>
 </html>
