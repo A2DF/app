fa63e657256da7ce678b597f7d1f946b5313423e
Hugo <Hugo@Hugo-PC>
2015-01-21 14:25:43 +0100

    popup

--- A2DF/ajoutClient.php
+++ A2DF/ajoutClient.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,initial-scale=1.0,user-scalable=no" />
-<link href="css/client.css" rel="stylesheet" type="text/css">
+<link href="css/formulaires.css" rel="stylesheet" type="text/css">
 <form method="post" action="traitement.php">
     <fieldset>
         <legend>Coordonnées</legend>
@@ -12,7 +12,7 @@
        <input type="text" name="prenom" id="prenom" />
        <br />
        <label for="adresse">Adresse :</label>
-       <textarea name="adresse" rows="5" id="adresse"></textarea>
+       <input type="text" name="adresse" id="prenom" />
        <br />
        <label for="cp">Code postal :</label>
        <input type="text" name="cp" id="cp" />
@@ -29,6 +29,7 @@
        <label for="cp">Téléphone portable :</label>
        <input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$">
     </fieldset>
+    <br />
     <input type="submit" name="envoi" value="Envoyer">
     <input type='reset' value='Annuler'>
    </p>
