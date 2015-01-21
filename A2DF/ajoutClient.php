714bccc8abdbfaa717acc398178499df8479c342
Alexandre <Alexandre@Altano-PC>
2015-01-21 15:24:50 +0100

    icons form users

--- A2DF/ajoutClient.php
+++ A2DF/ajoutClient.php
@@ -6,27 +6,35 @@
         <legend>Coordonnées</legend>
    <p>
        <label for="nom">Nom :</label>
+       <img src="img/tag_blue.png" width="16" height="16" alt="logo" />
        <input type="text" name="nom" id="nom" />
        <br />
        <label for="prenom">Prénom :</label>
+       <img src="img/tag_orange.png" width="16" height="16"  alt="logo" />
        <input type="text" name="prenom" id="prenom" />
        <br />
        <label for="adresse">Adresse :</label>
+       <img src="img/direction.png" width="16" height="16"  alt="logo" />
        <input type="text" name="adresse" id="prenom" />
        <br />
        <label for="cp">Code postal :</label>
+       <img src="img/counter.png" width="16" height="16"  alt="logo" />
        <input type="text" name="cp" id="cp" />
        <br />
        <label for="cp">Ville :</label>
+       <img src="img/church.png" width="16" height="16"  alt="logo" />
        <input type="text" name="ville" id="ville" />
        <br />
        <label for="cp">Courriel :</label>
+       <img src="img/contact_email.png" width="16" height="16"  alt="logo" />
        <input type="email" name="email">
        <br />
        <label for="cp">Téléphone fixe :</label>
+       <img src="img/telephone.png" width="16" height="16"  alt="logo" />
        <input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$">
        <br />
        <label for="cp">Téléphone portable :</label>
+       <img src="img/phone.png" width="16" height="16"  alt="logo" />
        <input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$">
     </fieldset>
     <br />
