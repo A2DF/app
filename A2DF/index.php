08d30a633abfdc7e62af81d724865f18f527445e
Alexandre <Alexandre@Altano-PC>
2015-01-27 09:44:17 +0100

    index changé

--- A2DF/index.php
+++ A2DF/index.php
@@ -8,7 +8,7 @@ include ('html/head.html');
         <section>
             <article>                
                 <center><h1>VERSION ALPHA !</h1></center>
-                <center><p>Seule la fonction <u>appels</u> est actuellement disponible.</p></center>
+                <center><p>Seule la fonction <u>appels</u> et <u>atelier</u> sont actuellement disponibles.</p></center>
 
                 <center><p>Veuillez contacter Hugo ou Alexandre en cas de problèmes.</p></center>
 
