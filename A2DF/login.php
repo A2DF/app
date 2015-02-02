eace58cb57530736b9eb90a4800e22a25d1933a2
Hugo <Hugo@Hugo-PC>
2015-02-02 16:36:04 +0100

    AUTH

--- A2DF/login.php
+++ A2DF/login.php
@@ -1,50 +1,43 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE html>
+<?php
+$user_valide = "admin";
+$pass_valide = "admin";
+$user = "";
+$pass = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $user = filter_input(INPUT_POST, 'user');
+    $pass = filter_input(INPUT_POST, 'pass');
+
+    if (($user_valide == $user) && ($pass_valide == $pass)) {
+        session_start();
+        $_SESSION['user'] = $user;
+        $_SESSION['pass'] = $pass;
+        header('location: index.php');
+    }
+}
+?>
 <html>
-	
-<head>
-	<title>The Login-Animated Website Template | Home :: w3layouts</title>
-		<meta charset="utf-8">
-		<link href="css/login.css" rel='stylesheet' type='text/css' />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-		<!--webfonts-->
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,300,600,700' rel='stylesheet' type='text/css'>
-		<!--//webfonts-->
-</head>
-<body>
-	
-				 <!-----start-main---->
-				<div class="login-form">
-					<div class="head">
-						<img src="img/mem2.jpg" alt=""/>
-						
-					</div>
-				<form>
-					<li>
-						<input type="text" class="text" value="USERNAME" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'USERNAME';}" ><a href="#" class=" icon user"></a>
-					</li>
-					<li>
-						<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}"><a href="#" class=" icon lock"></a>
-					</li>
-					<div class="p-container">
-								<label class="checkbox"><input type="checkbox" name="checkbox" checked><i></i>Remember Me</label>
-								<input type="submit" onclick="myFunction()" value="SIGN IN" >
-							<div class="clear"> </div>
-					</div>
-				</form>
-			</div>
-			<!--//End-login-form-->
-		  <!-----start-copyright---->
-   					<div class="copy-right">
-						<p>Template by <a href="http://w3layouts.com">w3layouts</a></p> 
-					</div>
-				<!-----//end-copyright---->
-		 		
-</body>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <link href="css/login.css" rel='stylesheet' type='text/css' />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="login-form">
+            <form action='login.php' method='POST' name='formLogin'>
+                <li>
+                    <input type="text" name="user" value="<?= $user ?>"><a class="icon user"></a>
+                </li>
+                <li>
+                    <input type="password" name="pass" value="<?= $pass ?>"><a class="icon lock"></a>
+                </li>
+                <input type="submit" value="LOG IN" >
+            </form>
+        </div>
+        <div class="copy-right">
+            <p></p> 
+        </div>
+    </body>
 </html>
