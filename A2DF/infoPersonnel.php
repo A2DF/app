014a5bcfb253523a08bb75a672db63e9164e01ad
Hugo <Hugo@Hugo-PC>
2015-02-05 12:11:58 +0100

    patch

--- A2DF/infoPersonnel.php
+++ A2DF/infoPersonnel.php
@@ -1,9 +1,8 @@
 <html>
-    <link rel="stylesheet" href="css/pikaday.css" />
     <?php
     include ('private/fonctions.php');
     date_default_timezone_set('UTC');
-    $today_int = date("Y-m-d");
+
     $idPersonnel = filter_input(INPUT_GET, "id");
 
     //Initialisation du compteur d'erreurs pour le contrôle du formulaire
@@ -62,7 +61,7 @@
 
         if ($erreurs === 0) {
             //Modification des données dans la table "Client"
-            modificationPersonnel($dateEmbauche_, $nom_, $prenom_, $adresse_, $cp_, $ville_, $courriel_, $tel_, $portable_, $numSecu_, $contrat_, $salaire_);
+            modificationPersonnel($idPersonnel, $dateEmbauche_, $nom_, $prenom_, $adresse_, $cp_, $ville_, $courriel_, $tel_, $portable_, $numSecu_, $contrat_, $salaire_);
             ?>
 
             <script language="javascript">
@@ -78,72 +77,72 @@
 
         $unPersonnel = unPersonnel($idPersonnel);
         foreach ($unPersonnel as $personnel) {
-            $prenom = $personnel['prenom'];
+            $prenom_ = $personnel['prenom'];
 
             if (isset($personnel['nom'])) {
-                $nom = $personnel['nom'];
+                $nom_ = $personnel['nom'];
             } else {
-                $nom = "";
+                $nom_ = "";
             }
 
-            if (isset($$personnel['adresse'])) {
-                $adresse = ($$personnel['adresse']);
+            if (isset($personnel['adresse'])) {
+                $adresse_ = ($personnel['adresse']);
             } else {
-                $adresse = "";
+                $adresse_ = "";
             }
 
             if (isset($personnel['cp'])) {
-                $cp = $personnel['cp'];
+                $cp_ = $personnel['cp'];
             } else {
-                $cp = "";
+                $cp_ = "";
             }
 
             if (isset($personnel['ville'])) {
-                $ville = $personnel['ville'];
+                $ville_ = $personnel['ville'];
             } else {
-                $ville = "";
+                $ville_ = "";
             }
 
             if (isset($personnel['courriel'])) {
-                $courriel = $personnel['courriel'];
+                $courriel_ = $personnel['courriel'];
             } else {
-                $courriel = "";
+                $courriel_ = "";
             }
 
             if (isset($personnel['tel'])) {
-                $tel = $personnel['tel'];
+                $tel_ = $personnel['tel'];
             } else {
-                $tel = "";
+                $tel_ = "";
             }
 
             if (isset($personnel['portable'])) {
-                $portable = $personnel['portable'];
+                $portable_ = $personnel['portable'];
             } else {
-                $portable = "";
+                $portable_ = "";
             }
 
             if (isset($personnel['dateEmbauche'])) {
-                $dateEmbauche = $personnel['dateEmbauche'];
+                $dateEmbauche_ = $personnel['dateEmbauche'];
             } else {
-                $dateEmbauche = "";
+                $dateEmbauche_ = "";
             }
 
             if (isset($personnel['numSecu'])) {
-                $numSecu = $personnel['numSecu'];
+                $numSecu_ = $personnel['numSecu'];
             } else {
-                $numSecu = "";
+                $numSecu_ = "";
             }
 
             if (isset($personnel['contrat'])) {
-                $contrat = $personnel['contrat'];
+                $contrat_ = $personnel['contrat'];
             } else {
-                $contrat = "";
+                $contrat_ = "";
             }
 
             if (isset($personnel['salaire'])) {
-                $dsalaire = $personnel['salaire'];
+                $salaire_ = $personnel['salaire'];
             } else {
-                $salaire = "";
+                $salaire_ = "";
             }
         }
     }
@@ -153,72 +152,61 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,initial-scale=1.0,user-scalable=no" />
         <link href="css/formulaires.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/pikaday.css" />
         <form method="post" action="infoPersonnel.php?id=<?php echo $idPersonnel ?>" autocomplete="off">
             <fieldset>
                 <legend>Coordonnées</legend>
-                <p>
-                    <label for="nom">Nom :</label>
-                    <img src="img/tag_blue.png" width="16" height="16"/>
-                    <input type="text" name="nom" value='<?php echo $nom ?>'/>
-                    <br />
-                    <label for="prenom">Prénom :</label>
-                    <img src="img/tag_orange.png" width="16" height="16"/>
-                    <input type="text" name="prenom" value='<?php echo $prenom ?>'/>
-                    <br />
-                    <label for="adresse">Adresse :</label>
-                    <img src="img/direction.png" width="16" height="16"/>
-                    <input type="text" name="adresse" value='<?php echo $adresse ?>'/>
-                    <br />
-                    <label for="cp">Code postal :</label>
-                    <img src="img/counter.png" width="16" height="16"/>
-                    <input type="text" name="cp" value='<?php echo $cp ?>'/>
-                    <br />
-                    <label for="cp">Ville :</label>
-                    <img src="img/church.png" width="16" height="16"/>
-                    <input type="text" name="ville" value='<?php echo $ville ?>'/>
-                    <br />
-                    <label for="cp">Courriel :</label>
-                    <img src="img/contact_email.png" width="16" height="16"/>
-                    <input type="email" name="courriel" value='<?php echo $courriel ?>'/>
-                    <br />
-                    <label for="cp">Téléphone fixe :</label>
-                    <img src="img/telephone.png" width="16" height="16"/>
-                    <input type="tel" name='tel' value='<?php echo $tel ?>' pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" />
-                    <br />
-                    <label for="cp">Téléphone portable :</label>
-                    <img src="img/phone.png" width="16" height="16"/>
-                    <input type="tel" name="portable" value='<?php echo $portable ?>' pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$"/>
+                <label for="nom">Nom :</label>
+                <img src="img/tag_blue.png" width="16" height="16"/>
+                <input type="text" name="nom" value='<?php echo $nom_ ?>'/>
+                <br />
+                <label for="prenom">Prénom :</label>
+                <img src="img/tag_orange.png" width="16" height="16"/>
+                <input type="text" name="prenom" value='<?php echo $prenom_ ?>'/>
+                <br />
+                <label for="adresse">Adresse :</label>
+                <img src="img/direction.png" width="16" height="16"/>
+                <input type="text" name="adresse" value='<?php echo $adresse_ ?>'/>
+                <br />
+                <label for="cp">Code postal :</label>
+                <img src="img/counter.png" width="16" height="16"/>
+                <input type="text" name="cp" value='<?php echo $cp_ ?>'/>
+                <br />
+                <label for="cp">Ville :</label>
+                <img src="img/church.png" width="16" height="16"/>
+                <input type="text" name="ville" value='<?php echo $ville_ ?>'/>
+                <br />
+                <label for="cp">Courriel :</label>
+                <img src="img/contact_email.png" width="16" height="16"/>
+                <input type="email" name="courriel" value='<?php echo $courriel_ ?>'/>
+                <br />
+                <label for="cp">Téléphone fixe :</label>
+                <img src="img/telephone.png" width="16" height="16"/>
+                <input type="tel" name='tel' value='<?php echo $tel_ ?>' pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" />
+                <br />
+                <label for="cp">Téléphone portable :</label>
+                <img src="img/phone.png" width="16" height="16"/>
+                <input type="tel" name="portable" value='<?php echo $portable_ ?>' pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$"/>
             </fieldset>
+            <br />
             <fieldset>
                 <legend>Entreprise</legend>
-                <table border="0">
-                    <tr>
-                        <td>Date d'embauche :</td>
-                        <img src="img/telephone.png" width="16" height="16"/>
-                        <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            echo "<td><input type='text' name='dateEmbauche' id='datepicker' value='" . $dateEmbauche_ . "' readonly></td>";
-                        } else {
-                            echo "<td><input type='text' name='dateEmbauche' id='datepicker' value='" . $today_int . "' readonly></td>";
-                        }
-                        ?>
-                    </tr>
-                    <tr>
-                        <td>Numéro de sécu :</td>
-                        <img src="img/telephone.png" width="16" height="16"/>
-                        <td><input type='text' name='numSecu' value='<?php echo $numSecu_ ?>'></td>
-                    </tr>
-                    <tr>
-                        <td>Type contrat :</td>
-                        <img src="img/telephone.png" width="16" height="16"/>
-                        <td><input type='text' name='contrat' value='<?php echo $contrat_ ?>'></td>
-                    </tr>
-                    <tr>
-                        <td>Salaire mensuel brut :</td>
-                        <img src="img/telephone.png" width="16" height="16"/>
-                        <td><input type='text' name='salaire' value='<?php echo $salaire_ ?>'></td>
-                    </tr>
-                </table>
+                <label>Date d'embauche :</label>
+                <img src="img/telephone.png" width="16" height="16"/>
+                <input type='text' name='dateEmbauche' id='datepicker' value='<?php echo $dateEmbauche_ ?>' readonly>
+                <br />
+                <label>Numéro de sécu :</label>
+                <img src="img/telephone.png" width="16" height="16"/>
+                <input type='text' name='numSecu' value='<?php echo $numSecu_ ?>'>
+                <br />
+                <label>Type contrat :</label>
+                <img src="img/telephone.png" width="16" height="16"/>
+                <input type='text' name='contrat' value='<?php echo $contrat_ ?>'>
+                <br />
+                <label>Salaire mensuel brut :</label>
+                <img src="img/telephone.png" width="16" height="16"/>
+                <input type='text' name='salaire' value='<?php echo $salaire_ ?>'>
+
             </fieldset>
             <br />
             <input type="submit" value="Enregistrer">
