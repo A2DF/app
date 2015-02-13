a584a75eda07de58cc307ddf5d34114c913452ed
Hugo <Hugo@Hugo-PC.home>
2015-02-13 22:59:36 +0100

    produit

--- A2DF/private/conf.php
+++ A2DF/private/conf.php
@@ -1,7 +1,7 @@
 <?php
 
 //Indentifiants MySQL
-$mysql_host = "mysql:host=192.168.0.32;dbname=a2df";
+$mysql_host = "mysql:host=localhost;dbname=a2df";
 $mysql_user = "a2df";
 $mysql_pass = "a2df";
 ?>
